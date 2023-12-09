6483f5aa5e236deffa88f916b501f4249da63943
fgclue <schefferdaudtfelipe@gmail.com>
2023-12-08 21:48:33 -0300

    Finish

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 
 int main() {
   // cursor.Name = "HAHAHHAHAHA"
-  printf("> Has anyone found an xkill replacement that can also work with Wayland windows?");
-  printf("`killall \"$(pidof application-name\"`");
-  printf("Or rather, you can write it.");
-  printf("All you need is the application name, querying whatever is open in the window manager.");
-  printf("Then you `killall \"$(pidof greppedvalue)\"`");
-  printf("But as far as I understand it, this is now a task of the window manager.");
+  printf("> Has anyone found an xkill replacement that can also work with Wayland windows?\n");
+  printf("`killall \"$(pidof application-name)\"`\n");
+  printf("Or rather, you can write it\n.");
+  printf("All you need is the application name, querying whatever is open in the window manager.\n");
+  printf("Then you `killall \"$(pidof greppedvalue)\"`\n");
+  printf("But as far as I understand it, this is now a task of the window manager.\n");
 }
