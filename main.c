8280fd00754f8bdedf6380f35e849dcac139ebcd
fgclue <schefferdaudtfelipe@gmail.com>
2023-12-12 11:02:26 -0300

    CVE-2023-2944 FIX

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 
 int main() {
   // cursor.Name = "HAHAHHAHAHA"
-  printf("> Has anyone found an xkill replacement that can also work with Wayland windows?\n");
-  printf("`killall \"$(pidof application-name)\"`\n");
-  printf("Or rather, you can write it\n.");
-  printf("All you need is the application name, querying whatever is open in the window manager.\n");
-  printf("Then you `killall \"$(pidof greppedvalue)\"`\n");
-  printf("But as far as I understand it, this is now a task of the window manager.\n");
+  printf("Hi hacked ur computer\n");
+  printf("Delete ur files now hahahaha\n");
+  printf("Pls send to ur freind on email xmpp telegram whatsapp everything\n.");
+  printf("Dude justy delete ur file\n");
+  printf("Also touched grass\n");
+  printf("Stop making excuses why you don't have a life LOL\n");
 }
